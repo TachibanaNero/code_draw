4498267cd0c2961fa67eaa4ab1f53b4c6df3ff1c
TachibanaNero <54711387+TachibanaNero@users.noreply.github.com>
2019-09-13 06:53:04 -0300

    v4

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 var htmlMenu = [];
 htmlMenu[0] = "<div id='teste'>BOA TARDE FAMILIA</div> <input id='press1' type='button' onclick='sumir()' value='Sumir'> <input id='press2' type='button' onclick='ver()' value='Ver'>";
-htmlMenu[1] = "div{background-color:red;};";
+htmlMenu[1] = '\n#teste{background-color:lime;\ntext-align: center;\n} \n';
 htmlMenu[2] = "function sumir(){ document.getElementById('teste').style.visibility='hidden'}; function ver(){ document.getElementById('teste').style.visibility='visible'};";
 
 
@@ -29,8 +29,16 @@ function editor() {
 
     code.open();
     code.writeln(
+      "<style>" +
+      cssEditor.getValue(cssEditor.setValue(cssEditor.getValue() +
+        htmlMenu[1])) +
+      "</style>" +
+      "<script>" +
+      jsEditor.getValue(jsEditor.setValue(jsEditor.getValue() +
+        htmlMenu[2])) +
+      "</script>" +
       htmlEditor.getValue(htmlEditor.setValue(htmlEditor.getValue() +
-        '<div>teste</div>'))
+        htmlMenu[0]))
     );
     code.close();
   };
