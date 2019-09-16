4383a73331c7f43b73299c7dc37be67f8a2cb994
TachibanaNero <54711387+TachibanaNero@users.noreply.github.com>
2019-09-16 01:13:41 -0300

    v2
    teste de menu

--- script_menu_teste.js
+++ script_menu_teste.js
@@ -1,8 +1,8 @@
 
 var htmlMenu = [];
-htmlMenu[0] = "<div id='teste'>BOA TARDE FAMILIA</div> <input id='press1' type='button' onclick='sumir()' value='Sumir'> <input id='press2' type='button' onclick='ver()' value='Ver'>";
+htmlMenu[0] = "<div id='teste'>BOA TARDE FAMILIA</div>\n<input id='press1' type='button' onclick='sumir()' value='Sumir'>\n<input id='press2' type='button' onclick='ver()' value='Ver'>";
 htmlMenu[1] = '\n#teste{background-color:lime;\ntext-align: center;\n} \n';
-htmlMenu[2] = "function sumir(){ document.getElementById('teste').style.visibility='hidden'}; function ver(){ document.getElementById('teste').style.visibility='visible'};";
+htmlMenu[2] = "function sumir(){ document.getElementById('teste').style.visibility='hidden'};\nfunction ver(){ document.getElementById('teste').style.visibility='visible'};";
 
 
 function editor() {
@@ -25,7 +25,7 @@ function editor() {
     code.close();
   };
 
-  document.getElementById('press').onclick = function () {
+  document.getElementById('demo').onclick = function myFunction () {
 
     code.open();
     code.writeln(
@@ -56,6 +56,14 @@ function editor() {
     );
     code.close();
   };
+}
+
+function myFunction1() {
+  if(demo.style.color == 'blue')
+    document.getElementById("demo").style.color = "red";
+    else{
+      document.getElementById("demo").style.color = "blue"
+    }
 }
 
 editor();
