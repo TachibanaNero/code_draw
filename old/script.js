3886a67e7904f9bbe2b7f72e4ab0621b8fa3c4b4
TachibanaNero <54711387+TachibanaNero@users.noreply.github.com>
2019-09-20 08:42:51 -0300

    Reorganização em pastas

--- old/script.js
+++ old/script.js
@@ -1,7 +1,9 @@
+
 var htmlMenu = [];
 htmlMenu[0] = "<div id='teste'>BOA TARDE FAMILIA</div> <input id='press1' type='button' onclick='sumir()' value='Sumir'> <input id='press2' type='button' onclick='ver()' value='Ver'>";
-htmlMenu[1] = "div{background-color:red;};";
+htmlMenu[1] = '\n#teste{background-color:lime;\ntext-align: center;\n} \n';
 htmlMenu[2] = "function sumir(){ document.getElementById('teste').style.visibility='hidden'}; function ver(){ document.getElementById('teste').style.visibility='visible'};";
+
 
 function editor() {
   var html = document.getElementById("html");
@@ -9,45 +11,50 @@ function editor() {
   var js = document.getElementById("js");
   var code = document.getElementById("code").contentWindow.document;
 
-  document.body.onkeyup = function () {
+  document.body.onload = function () {
     code.open();
     code.writeln(
       "<style>" +
-      css.value +
+      cssEditor.getValue() +
       "</style>" +
       "<script>" +
-      js.value +
-      "</script>" + html.value
+      jsEditor.getValue() +
+      "</script>" +
+      htmlEditor.getValue()
     );
     code.close();
   };
 
-  var meta = document.createElement('meta');
-  meta.httpEquiv = "Content-type";
-  meta.content = "text/html; charset=UTF-8";
-
-  var doc = document.getElementById('code').contentWindow.document.head;
-  debugger;
-  doc.append(meta);
-
   document.getElementById('press').onclick = function () {
 
-    document.getElementById("html").value += htmlMenu[0] + " ";
-    document.getElementsByClassName("css").value += htmlMenu[1] + " ";
-    document.getElementById("js").value += htmlMenu[2] + " ";
-
     code.open();
-    code.write(
-      html.value +
+    code.writeln(
       "<style>" +
-      css.value +
+      cssEditor.getValue(cssEditor.setValue(cssEditor.getValue() +
+        htmlMenu[1])) +
       "</style>" +
       "<script>" +
-      js.value +
-      "</script>"
+      jsEditor.getValue(jsEditor.setValue(jsEditor.getValue() +
+        htmlMenu[2])) +
+      "</script>" +
+      htmlEditor.getValue(htmlEditor.setValue(htmlEditor.getValue() +
+        htmlMenu[0]))
     );
     code.close();
+  };
 
+  document.body.onkeyup = function () {
+    code.open();
+    code.writeln(
+      "<style>" +
+      cssEditor.getValue() +
+      "</style>" +
+      "<script>" +
+      jsEditor.getValue() +
+      "</script>" +
+      htmlEditor.getValue()
+    );
+    code.close();
   };
 }
 
