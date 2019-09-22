a32cde8b7778663ebd5bff5c2ab4ed85955c2fec
TachibanaNero <54711387+TachibanaNero@users.noreply.github.com>
2019-09-22 12:31:21 -0300

    Revert "Reorganização em pastas" This reverts commit 3886a67e7904f9bbe2b7f72e4ab0621b8fa3c4b4.

--- old/script.js
+++ old/script.js
@@ -1,9 +1,7 @@
-
 var htmlMenu = [];
 htmlMenu[0] = "<div id='teste'>BOA TARDE FAMILIA</div> <input id='press1' type='button' onclick='sumir()' value='Sumir'> <input id='press2' type='button' onclick='ver()' value='Ver'>";
-htmlMenu[1] = '\n#teste{background-color:lime;\ntext-align: center;\n} \n';
+htmlMenu[1] = "div{background-color:red;};";
 htmlMenu[2] = "function sumir(){ document.getElementById('teste').style.visibility='hidden'}; function ver(){ document.getElementById('teste').style.visibility='visible'};";
-
 
 function editor() {
   var html = document.getElementById("html");
@@ -11,50 +9,45 @@ function editor() {
   var js = document.getElementById("js");
   var code = document.getElementById("code").contentWindow.document;
 
-  document.body.onload = function () {
-    code.open();
-    code.writeln(
-      "<style>" +
-      cssEditor.getValue() +
-      "</style>" +
-      "<script>" +
-      jsEditor.getValue() +
-      "</script>" +
-      htmlEditor.getValue()
-    );
-    code.close();
-  };
-
-  document.getElementById('press').onclick = function () {
-
-    code.open();
-    code.writeln(
-      "<style>" +
-      cssEditor.getValue(cssEditor.setValue(cssEditor.getValue() +
-        htmlMenu[1])) +
-      "</style>" +
-      "<script>" +
-      jsEditor.getValue(jsEditor.setValue(jsEditor.getValue() +
-        htmlMenu[2])) +
-      "</script>" +
-      htmlEditor.getValue(htmlEditor.setValue(htmlEditor.getValue() +
-        htmlMenu[0]))
-    );
-    code.close();
-  };
-
   document.body.onkeyup = function () {
     code.open();
     code.writeln(
       "<style>" +
-      cssEditor.getValue() +
+      css.value +
       "</style>" +
       "<script>" +
-      jsEditor.getValue() +
-      "</script>" +
-      htmlEditor.getValue()
+      js.value +
+      "</script>" + html.value
     );
     code.close();
+  };
+
+  var meta = document.createElement('meta');
+  meta.httpEquiv = "Content-type";
+  meta.content = "text/html; charset=UTF-8";
+
+  var doc = document.getElementById('code').contentWindow.document.head;
+  debugger;
+  doc.append(meta);
+
+  document.getElementById('press').onclick = function () {
+
+    document.getElementById("html").value += htmlMenu[0] + " ";
+    document.getElementsByClassName("css").value += htmlMenu[1] + " ";
+    document.getElementById("js").value += htmlMenu[2] + " ";
+
+    code.open();
+    code.write(
+      html.value +
+      "<style>" +
+      css.value +
+      "</style>" +
+      "<script>" +
+      js.value +
+      "</script>"
+    );
+    code.close();
+
   };
 }
 
